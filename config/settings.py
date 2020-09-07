aec4527246efb479de0ebd0b8f1dfbf9c86e9a7a
Júlia Aguiar <julia.aguiar10@outlook.com>
2020-09-07 00:35:40 -0300

    Alteracoes necessÃrias no settings

--- config/settings.py
+++ config/settings.py
@@ -132,13 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'config/core/static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILEs_DIRS = (os.path.join(BASE_DIR, 'config/core/static'),)
 
 
 #Arquivos de mídia
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "config", "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
-
-# from .local_settings import *
