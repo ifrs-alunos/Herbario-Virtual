61a2189b40da1156d102d4b3e238b0b9d5a7f503
Júlia Aguiar <julia.aguiar10@outlook.com>
2020-09-07 22:25:20 -0300

    Adicionando configuracoes do banco de dados postgresql no settings

--- config/settings.py
+++ config/settings.py
@@ -79,19 +79,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': os.environ.get('DB_NAME', 'config'),
-    #     'USER': os.environ.get('DB_USER', 'postgres'),
-    #     'PASSWORD': os.environ.get('DB_PASS', 'senhasegura'),
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }
-
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'herbario'),
+        'USER': os.environ.get('DB_USER', 'herbario'),
+        'PASSWORD': os.environ.get('DB_PASS', 'senhasegura'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    # }
 }
 
 
