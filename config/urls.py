942a40cb257911bdcddfdc967f5a637540dbde4b
Júlia Aguiar <julia.aguiar10@outlook.com>
2020-09-18 09:53:46 -0300

    Nenhuma nova mudanca relevante

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,11 +23,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('herbario/', include( 'herbarium.urls' ,namespace='herbarium')),
+    path('herbario/', include( 'herbarium.urls', namespace='herbarium')),
     path('', include( 'core.urls' ,namespace='core')),
-
 ]
-
 
 #Para Imagem ser servida pelo Django
 if settings.DEBUG:
