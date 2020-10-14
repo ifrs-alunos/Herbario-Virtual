ac87e17967d43540e63406e03da31f3c284f339b
Júlia Aguiar <julia.aguiar10@outlook.com>
2020-10-14 11:49:37 -0300

    Adicionando objetos no contexto e mostrando no template

--- core/views.py
+++ core/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Highlight
 
 # Create your views here.
 
 def index(request):
     template_name = 'core/index.html'
+    highlights = Highlight.objects.all()
+    # print(highlights)
 
-    return render(request, template_name, {})
+    # for i in highlights:
+    # 	print(i.text)
+
+    return render(request, template_name, {'destaques':highlights})
 
 def subjects(request):
     template_name = 'core/subjects.html'
