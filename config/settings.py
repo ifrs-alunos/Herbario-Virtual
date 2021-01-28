294f090e4af65f25d82c1e8a762fff4d8a89cbbf
Júlia Aguiar <julia.aguiar10@outlook.com>
2021-01-28 19:34:27 -0300

    Novos apps para interface administrativa

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'herbarium',
     'core',
+    'accounts',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +78,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.environ.get('DB_NAME', 'herbario'),
+    #     'USER': os.environ.get('DB_USER', 'herbario'),
+    #     'PASSWORD': os.environ.get('DB_PASS', 'senhasegura'),
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME', 'herbario'),
