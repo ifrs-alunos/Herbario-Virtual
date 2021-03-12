92dddc7f8f37ac4c66d038b5b7e16496af724885
Júlia Aguiar <julia.aguiar10@outlook.com>
2021-03-11 21:15:31 -0300

    Mudancas no estilo de formularios

--- accounts/models/profile.py
+++ accounts/models/profile.py
@@ -5,5 +5,5 @@ class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     name = models.CharField(max_length=200, verbose_name="Nome Completo")
     institution = models.CharField(max_length=150, verbose_name="Instituição onde trabalha/estuda")
-    role = models.CharField(max_length=100, verbose_name="Cargo de ofício", help_text="Utilize como base seu cargo na instituição citada acima.")
+    role = models.CharField(max_length=100, verbose_name="Cargo de ofício", help_text="Utilize como base seu cargo na instituição citada.")
     phone = models.CharField(max_length=11, verbose_name="Telefone")
