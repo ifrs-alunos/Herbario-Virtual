5cd91c56f062638749f9158951e30877a3a301bd
Júlia Aguiar <julia.aguiar10@outlook.com>
2021-04-01 17:09:32 -0300

    Desenvolvendo perfil e modificacoes form cadastro

--- accounts/forms/user_form.py
+++ accounts/forms/user_form.py
@@ -7,6 +7,14 @@ from django.contrib.auth.models import User
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="E-mail")
 
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        
+        self.fields['password1'].help_text = "Mínimo 8 caracteres. Não use dados pessoais, senhas comuns ou somente números."
+        self.fields['username'].help_text = "Credencial utilizada para login. Use letras, números e os símbolos @/./+/-/_ apenas."
+        
+    # password1 = forms.CharField(label="Senha", help_text="Mínimo 8 caracteres. É proibido somente números, dados pessoais ou senhas comuns")
+
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('email',)
--- accounts/models/profile.py
+++ accounts/models/profile.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, verbose_name="Nome completo")
-    institution = models.CharField(max_length=150, verbose_name="Instituição de trabalho e/ou estudo")
+    name = models.CharField(max_length=200, verbose_name="Nome completo", help_text="Não utilize abreviações nos sobrenomes.")
+    institution = models.CharField(max_length=150, verbose_name="Instituição de trabalho e/ou estudo", help_text="Caso possuia mais de uma, escreva a que atua principalmente.")
     role = models.CharField(max_length=100, verbose_name="Cargo de ofício", help_text="Utilize como base seu cargo na instituição citada.")
-    phone = models.CharField(max_length=11, verbose_name="Telefone fixo ou celular")
+    phone = models.CharField(max_length=11, verbose_name="Telefone fixo ou celular", help_text="Informe o DDD e, em seguida, seu número.")
