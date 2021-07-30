ad7a3a23d3c336fe90df6f602f53d5b9ca141e71
luisa <luisamarcolin10@gmail.com>
2021-07-29 21:20:22 -0300

    Arquivos removidos para relocação

--- dashboard/forms/photo_solicitation_form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from accounts.models import PhotoSolicitation
-
-class PhotoSolicitationModelForm(forms.ModelForm):
-    '''Formulário acessado por contribuidores para enviar uma solicitação de nova foto de planta'''
-
-    class Meta:
-        model = PhotoSolicitation
-        fields = '__all__'
--- dashboard/forms/plant_solicitation_form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from accounts.models import PlantSolicitation
-
-class PlantSolicitationModelForm(forms.ModelForm):
-    '''Formulário acessado por contribuidores para enviar uma solicitação de planta nova'''
-
-    class Meta:
-        model = PlantSolicitation
-        fields = '__all__'
