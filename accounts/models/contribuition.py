f4f7f8fe8d415763108ca327d8b95363f83a8d59
luisa <luisamarcolin10@gmail.com>
2021-07-29 21:23:13 -0300

    Arquivo removido para mudança de funcionamento da ferramenta. Será utilizado novo método para painel de contribuições.

---
+++ accounts/models/contribuition.py
@@ -0,0 +1,7 @@
+from django.db import models
+from . profile import Profile
+
+class Contribuition(models.Model):
+    """Essa classe especifica os dados referentes a uma contribuição"""
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="contribuitions", verbose_name="Perfil")
