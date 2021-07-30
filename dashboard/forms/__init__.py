41bbc8ad676213c275b5fcf0ef6b2cbeaa58fca1
luisa <luisamarcolin10@gmail.com>
2021-07-29 21:20:22 -0300

    Arquivos removidos para relocação

--- dashboard/forms/__init__.py
+++
@@ -1,3 +0,0 @@
-from .solicitation_update_form import SolicitationStatusUpdateForm
-from .plant_solicitation_form import PlantSolicitationModelForm
-from .photo_solicitation_form import PhotoSolicitationModelForm
--- dashboard/forms/solicitation_update_form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from accounts.models import Solicitation
-
-class SolicitationStatusUpdateForm(forms.ModelForm):
-    """Essa classe é utilizada para negar ou aceitar uma solicitação, isto é, mudar o seu status"""
-
-    class Meta:
-        model = Solicitation
-        fields = ['status']
