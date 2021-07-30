b62ccd82db4d4d3303fa6dc382a926f9db9d9602
luisa <luisamarcolin10@gmail.com>
2021-07-29 21:21:14 -0300

    Arquivos relocados e/ou editados para app 'accounts'

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,21 @@ app_name = 'accounts'
 urlpatterns = [
     path('criar/', views.create_user, name='create'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('perfil/', views.ProfileView.as_view(), name='profile'),
+    # path('perfil/', views.ProfileView.as_view(), name='profile'),
     path('sair/', auth_views.LogoutView.as_view(), name='logout'),
     path('sobre/', views.InfoView.as_view(), name='info'),
+
+    path('', views.DashboardView.as_view(), name='view_dashboard'),
+    path('perfil/', views.update_profile, name='profile'),
+    path('solicitacao/', views.SolicitationCreateView.as_view(), name='solicitation'),
+    path('lista-solicitacoes/', views.SolicitationListView.as_view(), name='solicitation_list'),
+    path('solicitacao/<int:pk>/', views.SolicitationUpdateView.as_view(), name="solicitation_update"),
+    path('trocar-senha', views.ChangePassword.as_view(), name="change_password"),
+    path('contribuicoes', views.ContributionTemplateView.as_view(), name="contributions"),
+    path('atualizar-herbario/', views.HerbariumListView.as_view(), name="herbarium_update"),
+    path('lista-usuarios/', views.UserListView.as_view(), name="user_list"),
+    path('solicitacao-de-planta/', views.plant_solicitation, name="plant_solicitation"),
+    path('solicitacao-de-foto/', views.photo_solicitation, name="photo_solicitation"),
+    path('solicitacoes-plantas/', views.PlantSolicitationListView.as_view(), name="plant_solicitation_list"),
+    path('solicitacoes-fotos/', views.PhotoSolicitationListView.as_view(), name="photo_solicitation_list"),
 ]
