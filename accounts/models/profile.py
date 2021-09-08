c83e11873ea8bb9e12ad45024d33f3c504137f9a
luisa <luisamarcolin10@gmail.com>
2021-09-08 19:30:10 -0300

    Correção de erros

--- accounts/models/profile.py
+++ accounts/models/profile.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from .solicitation import Solicitation
 
 class Profile(models.Model):
     """Essa classe define um perfil de um usuário"""
     
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, verbose_name="Nome completo", help_text="Não utilize abreviações nos sobrenomes.")
     institution = models.CharField(max_length=150, verbose_name="Instituição de trabalho e/ou estudo", help_text="Caso possuia mais de uma, escreva a que atua principalmente.")
     role = models.CharField(max_length=100, verbose_name="Cargo de ofício", help_text="Utilize como base seu cargo na instituição citada.")
