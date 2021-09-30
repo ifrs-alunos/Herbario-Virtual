4569cad39903df4e170fda17f46389598811d4e3
luisa <luisamarcolin10@gmail.com>
2021-09-30 16:06:13 -0300

    Alterados arquivos para formulário de condições e culturas

--- accounts/forms/__init__.py
+++ accounts/forms/__init__.py
@@ -7,3 +7,4 @@ from .solicitation_update_form import SolicitationStatusUpdateForm
 from .plant_solicitation_form import PlantSolicitationModelForm
 from .disease_solicitation_form import DiseaseSolicitationModelForm
 from .disease_char_form import DiseaseCharSolicitationModelForm
+from .culture_solicitation_form import CultureSolicitationModelForm
--- disease/forms/__init__.py
+++ disease/forms/__init__.py
@@ -1,2 +1,3 @@
 from .disease_form import DiseaseForm
 from .search_form import SearchForm
+from .conditions_form import ConditionForm
