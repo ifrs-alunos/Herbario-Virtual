39e6070f1c4cc4875af23d6983a4b7aa5d6e9972
Ricardo Luis dos Santos <ricardosanbr@gmail.com>
2021-11-10 17:09:13 -0300

    Update models.py

--- disease/models.py
+++ disease/models.py
@@ -103,7 +103,7 @@ class Disease(models.Model):
     # Booleando representando o estado de publicação desta doença
     published_disease = models.BooleanField(verbose_name="Publicado", null=True)
 
-    characteristic = models.ForeignKey("accounts.CharSolicitationModel", on_delete=models.SET_NULL, blank=True, null=True)
+    characteristic = models.ForeignKey("accounts.CharSolicitationModel", on_delete=models.SET_NULL, default=None, blank=True, null=True)
 
     
     def __str__(self):
