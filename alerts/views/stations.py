3dafd7a80a3aaa3d4c28027ac62ddd419d8bfe4a
leofmichelon34@gmail.com <leofmichelon34@gmail.com>
2021-12-17 13:40:53 -0300

    renomear arquivos de template pra station_*.html

--- alerts/views/stations.py
+++ alerts/views/stations.py
@@ -1,12 +1,10 @@
-from django.db.models import Q
-from django.shortcuts import render
 from ..models import Station
 from django.views.generic import ListView, DetailView
 
 
 class StationIndex(ListView):
     model = Station
-    template_name = 'index.html'
+    template_name = 'station_index.html'
     context_object_name = 'stations'
 
     paginate_by = 12
@@ -22,7 +20,7 @@ class StationIndex(ListView):
 class StationDetail(DetailView):
     # Mostra detalhes de uma estação específica. Passa no contexto os dados de uma estação
     model = Station
-    template_name = 'detail.html'
+    template_name = 'station_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
