cdc5524cb27933363ecf0826dbed350d6493b779
leofmichelon34@gmail.com <leofmichelon34@gmail.com>
2021-12-22 11:57:18 -0300

    substituir view de gráficos por view baseada em classe

--- alerts/urls.py
+++ alerts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from .views import save_report, board_notify
-from .views import render_graph
 from .views import stations
 
 app_name = 'alerts'
@@ -11,5 +10,4 @@ urlpatterns = [
     path('board_notify/', board_notify, name="board_notify"),
     path('estacoes/', stations.StationIndex.as_view(), name="stations"),
     path('estacoes/<slug:slug>', stations.StationDetail.as_view(), name="station_detail"),
-    path('graph/<str:station_id>', render_graph, name="render_graph")
 ]
