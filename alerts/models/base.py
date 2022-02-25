885985aae990dc61b19635836f8335f745ff42ed
leofmichelon34@gmail.com <leofmichelon34@gmail.com>
2022-02-24 23:19:03 -0300

    mais tratamento de erros

--- alerts/models/base.py
+++ alerts/models/base.py
@@ -3,11 +3,14 @@ from typing import Tuple
 
 
 class BaseModel(Model):
-    def get_fields(self, exclude: Tuple[str] = ()) -> list:
+    def get_fields(self, exclude: Tuple[str] = None) -> list:
         """
         :param exclude: campos que serão excluídos da lista retornada
         :return: lista de todos os campos do objeto
         """
+        if not exclude:
+            exclude = ()
+
         return [i.name for i in self._meta.fields if i.name not in exclude]
 
     class Meta:
