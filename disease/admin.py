062aa601dbc277af8e0e25e5263f3665495d13ba
Guilherme Alves da Silva <guilherme1077@gmail.com>
2022-07-04 23:04:17 -0300

    tirando o constant do disease

--- disease/admin.py
+++ disease/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
 
 class PhotoInline(admin.TabularInline):
-    model = PhotoDisease
+	model = PhotoDisease
+
 
 class DiseaseAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name_disease',)}
-    list_display = ('name_disease', 'scientific_name_disease','culture_disease',)
-    list_filter = ('culture_disease',)
-    search_fields = ('name_disease', 'scientific_name_disease', 'symptoms_disease')
-    inlines = [PhotoInline, ]
+	prepopulated_fields = {'slug': ('name_disease',)}
+	list_display = ('name_disease', 'scientific_name_disease', 'culture_disease',)
+	list_filter = ('culture_disease',)
+	search_fields = ('name_disease', 'scientific_name_disease', 'symptoms_disease')
+	inlines = [PhotoInline, ]
+
 
 admin.site.register(Region)
 admin.site.register(State)
@@ -19,5 +22,4 @@ admin.site.register(Disease, DiseaseAdmin)
 admin.site.register(Culture)
 admin.site.register(ConditionSolicitation)
 admin.site.register(Condition)
-admin.site.register(Constant)
 admin.site.register(PhotoDisease)
