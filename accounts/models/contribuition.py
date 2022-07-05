9025d994642c1b97eeaa8c1108f20040a28dc7a3
Guilherme Alves da Silva <guilherme1077@gmail.com>
2022-07-04 23:00:19 -0300

    mudando os models de accounts

--- accounts/models/contribuition.py
+++ accounts/models/contribuition.py
@@ -4,4 +4,5 @@ from . profile import Profile
 class Contribuition(models.Model):
     """Essa classe especifica os dados referentes a uma contribuição"""
 
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="contribuitions", verbose_name="Perfil")
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="contribuitions",
+                                verbose_name="Perfil",default=Profile.objects.get(pk=1))
