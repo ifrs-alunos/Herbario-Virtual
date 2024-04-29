81c89a201ca2a198fdaf8c675e7a3d9dec5b7a6a
leo <fmleonardo@protonmail.com>
2024-04-29 17:52:17 -0300

    rename send_whatsapp_message to send_message and create convert_phone_number

--- alerts/whatsapp.py
+++ alerts/whatsapp.py
@@ -3,7 +3,7 @@ import requests
 from alerts.constants import WHATSAPP_API_URL
 
 
-def send_whatsapp_message(phone_number: str, message: str) -> dict:
+def send_message(phone_number: str, message: str) -> dict:
     """
     Sends a request to the whatsapp-web.js API to send a message to a phone number.
     http://localhost:3000/api-docs/#/default/post_sendMessage
@@ -22,3 +22,16 @@ def send_whatsapp_message(phone_number: str, message: str) -> dict:
     response = requests.post(WHATSAPP_API_URL + "/sendMessage", json=body)
 
     return response.json()
+
+
+def convert_phone_number(phone_number: str) -> str:
+    """
+    Converts a phone number to the format required by the whatsapp-web.js API.
+    Args:
+        phone_number: Whatsapp phone number
+
+    Returns:
+        The phone number in the format required by the API.
+    """
+
+    return f"55{phone_number[0:2]}{phone_number[3:]}"
