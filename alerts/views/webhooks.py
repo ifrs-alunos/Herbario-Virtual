96c9efea34b886cf380c41afd8863706ff93e73e
leo <fmleonardo@protonmail.com>
2024-05-13 15:00:48 -0300

    feat: implement whatsapp verification webhook

--- alerts/views/webhooks.py
+++ alerts/views/webhooks.py
@@ -1,7 +1,14 @@
 import json
+from datetime import timedelta
 
+from django import urls
 from django.http import JsonResponse
+from django.urls import reverse
+from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
+
+from alerts.models.whatsapp import MessageConfirmation, CODE_EXPIRATION_TIME, CODE_LENGTH
+from .. import whatsapp
 
 
 @csrf_exempt
@@ -11,10 +18,24 @@ def whatsapp(request):
         # https://docs.wwebjs.dev/Message.html
         data = json.loads(request.body)
 
-        print(data)
+        code: str | None = data.get("body")
+        if not (isinstance(code, str) and (len(code) == CODE_LENGTH and code.isnumeric())):
+            whatsapp.send_message(data.get("from").split("@")[0],
+                                  "Código inválido. Por favor, digite um código de 6 dígitos.")
+            return JsonResponse({"message": "invalid verification code"}, status=400)
 
-        body: str | None = data.get("body")
-        if isinstance(body, str) and (len(body) == 6 and body.isnumeric()):
-            ...
+        mc = MessageConfirmation.objects.filter(code=code,
+                                                created_at__gt=timezone.now() - timedelta(minutes=CODE_EXPIRATION_TIME),
+                                                verified=False).first()
+
+        if not MessageConfirmation.objects.exists():
+            whatsapp.send_message(data.get("from").split("@")[0],
+                                  f"O código enviado não existe ou já expirou.\n"
+                                  f"Por favor gere outro código em "
+                                  f"{request.build_absolute_uri(reverse('dashboard:profile'))}")
+            return JsonResponse({"message": "verification code not found"}, status=400)
+
+        mc.verified = True
+        mc.save()
 
     return JsonResponse({"message": "ok"})
