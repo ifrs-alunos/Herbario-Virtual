42665f3ff7c216690f4e11c9aa03c401998b7e89
leo <fmleonardo@protonmail.com>
2024-06-03 18:01:56 -0300

    refactor: start class based view for receiving reports

--- alerts/views/report.py
+++ alerts/views/report.py
@@ -1,15 +1,34 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+import json
 
-# from .utils import create_report_from_params
-#
-#
-# # views
-#
-# @csrf_exempt
-# def save_report(request):
-#     params = request.POST
-#
-#     report = create_report_from_params(params)
-#
-#     return HttpResponse(":)")
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+
+from alerts.models import Report, Sensor, Station
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class ReportView(View):
+    def post(self, request):
+        # POST body looks like this:
+        """
+        {
+          "chipid":185249135999496,
+          "readings": [
+            { "sensor_name": "dht_h", "value": 57.5 },
+            { "sensor_name": "dht_t", "value": 17.2 },
+            { "sensor_name": "rain", "value": 0 }
+          ]
+        }
+        """
+
+        body = json.loads(request.body.decode("utf-8"))
+
+        station = Station.objects.get(station_id=body.get("chipid"))
+
+        for reading in body.get("readings"):
+            sensor = station.sensor_set.get(type__name=reading.get("sensor_name"))
+            Report.objects.create(sensor=sensor, value=reading.get("value"))
+
+        return JsonResponse({"message": "ok"})
