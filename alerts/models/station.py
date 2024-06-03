748236dd292ea40b3a31e89f7526d914792238e6
leo <fmleonardo@protonmail.com>
2024-06-03 18:02:21 -0300

    fix: change station_id to a charfield

--- alerts/models/station.py
+++ alerts/models/station.py
@@ -4,12 +4,13 @@ from django.db import models
 
 
 class Station(BaseModel):
-    station_id = models.IntegerField(verbose_name="Identificador da estação")
+    station_id = models.CharField(max_length=20, verbose_name="Identificador da estação")
     slug = models.SlugField(unique=True, null=True)
     alias = models.CharField(max_length=100, verbose_name="Nome", null=True, blank=True)
     lat_coordinate = models.FloatField(verbose_name="Latitude", null=True)
     lon_coordinate = models.FloatField(verbose_name="Longitude", null=True)
-    description = models.TextField('Descrição da estação', max_length=400, help_text='Endereço, ponto de referência, responsável...', null=True)
+    description = models.TextField('Descrição da estação', max_length=400,
+                                   help_text='Endereço, ponto de referência, responsável...', null=True)
 
     @property
     def lat_lon(self):
@@ -23,7 +24,7 @@ class Station(BaseModel):
     def get_human_sensor_condition(self):
         for sensor in self.sensor_set.all():
             if sensor.report_set.last():
-                sensor_report = sensor.report_set.last() # Último report do sensor
+                sensor_report = sensor.report_set.last()  # Último report do sensor
                 if sensor.type.metric == 'bool':
                     sensor_value = float(sensor_report.value)
                     if sensor_value == 1.00:
@@ -32,8 +33,9 @@ class Station(BaseModel):
                         return False
 
     def get_mathmodels_id(self):
-        mathmodels_id = self.mathmodel_set.all().values_list("id",flat=True)
+        mathmodels_id = self.mathmodel_set.all().values_list("id", flat=True)
         return list(mathmodels_id)
+
     def __str__(self):
         return f"{self.alias} {self.lat_lon}"
 
