0efa8d4ec807d7ac9413b4f1a0f180cdadb0c308
leo <fmleonardo@protonmail.com>
2024-06-03 18:02:38 -0300

    feat: add view to urls.py

--- alerts/urls.py
+++ alerts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views.map import get_map
 
-from .views import board_notify, formula, view_graphs, collect_data_cepadi, get_sensor_data, collect_data_stationif, \
+from .views import board_notify, formula, report, view_graphs, collect_data_cepadi, get_sensor_data, \
+    collect_data_stationif, \
     get_station_sensors_data, download_data_station, get_station, get_mathmodels, get_station_mathmodel_color, webhooks
 from .views import mathmodel
+
 app_name = 'alerts'
 
 urlpatterns = [
@@ -18,12 +20,13 @@ urlpatterns = [
     path('mapa', get_map, name='map_url'),
     path('visualizar/graficos', view_graphs, name="view_graphs"),
     path('coletar_dados/cepadi', collect_data_cepadi, name="collect_data_cepadi"),
-    path('coletar_dados/estacaoif', collect_data_stationif, name="collect_data_stationif"),
+    path('coletar_dados', report.ReportView.as_view(), name="collect_data"),
     path('get/sensor/<int:sensor_id>/<str:date_filter>/data', get_sensor_data, name="get_sensor_data"),
     path('get/estacao/<int:station_id>/sensores', get_station_sensors_data, name="get_station_data"),
     path('baixar/dados/estacao/<int:station_id>', download_data_station, name="dowload_data_station"),
     path('get/estacao/<int:station_id>', get_station, name="get_station"),
     path('get/mathmodels/<str:date_filter>', get_mathmodels, name="get_mathmodels"),
-    path('get/station/<int:station_id>/mathmodel/<int:mathmodel_id>/color', get_station_mathmodel_color, name="get_station_mathmodel_color"),
+    path('get/station/<int:station_id>/mathmodel/<int:mathmodel_id>/color', get_station_mathmodel_color,
+         name="get_station_mathmodel_color"),
     path('webhooks/whatsapp', webhooks.whatsapp, name="whatsapp")
 ]
