065b4407d35a776e46367511be92fb4c1dd2f2f8
leo <fmleonardo@protonmail.com>
2024-07-02 21:42:49 -0300

    refactor: use mixins, make views model calculation generic
    mixins para evitar código repetido em views de create e update
    código do cálculo de modelos agora deve funcionar de forma genérica, baseado nos sensores de cada estação, as fórmulas agora devem usar o nome de cada sensor (possivelmente alterar mais pra frente)

--- dashboard/views/alerts.py
+++ dashboard/views/alerts.py
@@ -1,3 +1,4 @@
+import re
 from itertools import groupby, chain
 
 import numexpr
@@ -16,6 +17,82 @@ from operator import attrgetter
 from alerts.models import MathModel, Sensor, Constant, MathModelResult, Report
 
 
+def _create_constants(constants_string: str, saved_form) -> None:
+    constants = constants_string.split(";")
+    for constant in constants:
+        if constant != '':
+            name, value = constant.split("=")
+            new_constant = Constant(name=name, value=value, mathmodel=saved_form)
+            new_constant.save()
+
+
+def _replace_values(exp: str, values_dict: dict) -> str:
+    # Tokens se refere a cada "elemento" na expressão, seja um operador, um número, uma variável, etc.
+    tokens = re.split(r'(\W+)', exp)
+
+    new_tokens = [values_dict.get(token, token) for token in tokens]
+
+    return "".join(new_tokens)
+
+
+class MathModelFormMixin:
+    model = MathModel
+    form_class = MathModelForm
+
+    template_name = 'dashboard/mathmodel_create_form.html'
+    success_url = reverse_lazy('dashboard:mathmodel_update')
+
+    def validate_form(self, form: MathModelForm) -> None:
+        math_model: MathModel = form.save()
+        math_model.constant_set.all().delete()
+        math_model.mathmodelresult_set.all().delete()
+        _create_constants(form.cleaned_data['constants'], math_model)
+
+        constants_dict = {name: str(value) for name, value in math_model.constant_set.values_list('name', 'value')}
+
+        exp = _replace_values(math_model.source_code, constants_dict)
+
+        list_results = []
+
+        for station in math_model.stations.all():
+            """
+            reports_by_hour é um dicionário no seguinte formato:
+            {
+                datetime1: { sensor1: value1, sensor2: value2, ... },
+                datetime2: { sensor1: value1, sensor2: value2, ... },
+                ...
+            """
+            reports_by_hour = {}
+
+            for sensor in station.sensor_set.all():
+                name = sensor.type.name
+
+                reports = sensor.report_set \
+                    .annotate(hour=TruncHour('time', is_dst=False)) \
+                    .values('hour') \
+                    .annotate(avg_value=Avg('value')) \
+                    .order_by("hour")
+
+                for report in reports:
+                    hour = report['hour']
+
+                    if reports_by_hour.get(hour) is None:
+                        reports_by_hour[hour] = {}
+
+                    reports_by_hour[hour][name] = report['avg_value']
+
+            condition = 0
+
+            for hour, reports in reports_by_hour.items():
+                new_exp = _replace_values(exp, reports)
+                report_calc = numexpr.evaluate(new_exp).astype(float)
+
+                condition = condition + report_calc
+
+                list_results.append(MathModelResult(value=condition, date=localtime(hour), mathmodel=math_model))
+        MathModelResult.objects.bulk_create(list_results)
+
+
 class MathModelListView(ListView):
     model = MathModel
     context_object_name = 'mathmodels'
@@ -30,75 +107,14 @@ class MathModelListView(ListView):
         return data
 
 
-class CreateMathModel(CreateView):
-    model = MathModel
-    form_class = MathModelForm
-    template_name = 'dashboard/mathmodel_create_form.html'
-    success_url = reverse_lazy('dashboard:mathmodel_update')
-
+class CreateMathModel(CreateView, MathModelFormMixin):
     def form_valid(self, form):
-        saved_form = form.save()
-        constants = form.cleaned_data['constants'].split(";")
-        for constant in constants:
-            if constant != '':
-                constant_string = constant.split("=")
-                name = constant_string[0]
-                value = constant_string[1]
-                new_constant = Constant(name=name, value=value, mathmodel=saved_form)
-                new_constant.save()
-
-        exp = saved_form.source_code
-        new_exp = ""
-        for key, value in saved_form.constant_set.all().values_list('name', 'value'):
-            exp = exp.replace(key, str(value))
-
-        list_results = []
-        for station in saved_form.stations.all():
-            sensor_t = station.sensor_set.get(type__metric="C")
-            sensor_rh = station.sensor_set.get(type__metric="%")
-            reports_sensor_t = sensor_t.report_set.annotate(
-                value_float=Cast('value', output_field=FloatField())) \
-                .annotate(hour=TruncHour('time', is_dst=False)) \
-                .values('hour') \
-                .annotate(avg_value=Avg('value_float')) \
-                .order_by("hour")
-
-            reports_sensor_rh = sensor_rh.report_set.annotate(
-                value_float=Cast('value', output_field=FloatField())) \
-                .annotate(hour=TruncHour('time', is_dst=False)) \
-                .values('hour') \
-                .annotate(avg_value=Avg('value_float')) \
-                .order_by("hour")
-            zipped_models = zip(reports_sensor_t, reports_sensor_rh)
-            condition = 0
-            for temp, rh in zipped_models:
-                hour = temp["hour"]
-                new_exp = exp
-                if float(rh['avg_value']) >= 85 and 10 < float(temp['avg_value']) < 26.5:
-                    if "t" in exp:
-                        new_exp = new_exp.replace('t', str(temp['avg_value']))
-                    if "rh" in exp:
-                        new_exp = new_exp.replace('rh', str(rh['avg_value']))
-                    print(new_exp)
-                    report_calc = numexpr.evaluate(new_exp).astype(float)
-                    condition = condition + report_calc
-                    print(report_calc,condition)
-                else:
-                    condition = 0
-
-                list_results.append(MathModelResult(value=condition, date=localtime(hour), mathmodel=saved_form))
-
-        MathModelResult.objects.bulk_create(list_results)
+        self.validate_form(form)
 
         return super().form_valid(form)
 
 
-class UpdateMathModel(UpdateView):
-    model = MathModel
-    form_class = MathModelForm
-    template_name = 'dashboard/mathmodel_create_form.html'
-    success_url = reverse_lazy('dashboard:mathmodel_update')
-
+class UpdateMathModel(UpdateView, MathModelFormMixin):
     def get_initial(self):
         initial = super().get_initial()
         constants = Constant.objects.all().filter(mathmodel=self.object)
@@ -110,58 +126,10 @@ class UpdateMathModel(UpdateView):
         return initial
 
     def form_valid(self, form):
-        saved_form = form.save()
-        saved_form.constant_set.all().delete()
-        saved_form.mathmodelresult_set.all().delete()
-        constants = form.cleaned_data['constants'].split(";")
-        for constant in constants:
-            if constant != '':
-                constant_string = constant.split("=")
-                name = constant_string[0]
-                value = constant_string[1]
-                new_constant = Constant(name=name, value=value, mathmodel=saved_form)
-                new_constant.save()
-
-        exp = saved_form.source_code
-        new_exp = ""
-        for key, value in saved_form.constant_set.all().values_list('name', 'value'):
-            exp = exp.replace(key, str(value))
-        list_results = []
-        for station in saved_form.stations.all():
-            sensor_t = station.sensor_set.get(type__metric="C")
-            sensor_rh = station.sensor_set.get(type__metric="%")
-            reports_sensor_t = sensor_t.report_set.annotate(
-                value_float=Cast('value', output_field=FloatField())) \
-                .annotate(hour=TruncHour('time', is_dst=False)) \
-                .values('hour') \
-                .annotate(avg_value=Avg('value_float')) \
-                .order_by("hour")
-
-            reports_sensor_rh = sensor_rh.report_set.annotate(
-                value_float=Cast('value', output_field=FloatField())) \
-                .annotate(hour=TruncHour('time', is_dst=False)) \
-                .values('hour') \
-                .annotate(avg_value=Avg('value_float')) \
-                .order_by("hour")
-            zipped_models = zip(reports_sensor_t, reports_sensor_rh)
-            condition = 0
-            for temp, rh in zipped_models:
-                hour = temp["hour"]
-                new_exp = exp
-                if float(rh['avg_value']) >= 85 and 10 < float(temp['avg_value']) < 26.5:
-                    if "t" in exp:
-                        new_exp = new_exp.replace('t', str(temp['avg_value']))
-                    if "rh" in exp:
-                        new_exp = new_exp.replace('rh', str(rh['avg_value']))
-                    report_calc = numexpr.evaluate(new_exp).astype(float)
-                    condition = condition + report_calc
-                else:
-                    condition = 0
-
-                list_results.append(MathModelResult(value=condition, date=localtime(hour), mathmodel=saved_form))
-        MathModelResult.objects.bulk_create(list_results)
+        self.validate_form(form)
 
         return super().form_valid(form)
+
 
 class DeleteMathModel(DeleteView):
     model = MathModel
