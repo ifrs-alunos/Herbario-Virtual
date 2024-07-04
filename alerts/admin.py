ab435f98c0275d911b00fa77b2245b4286503f03
leo <fmleonardo@protonmail.com>
2024-07-04 00:37:10 -0300

    fix: undo report ordering change
    muita coisa dependia da ordem estar assim

--- alerts/admin.py
+++ alerts/admin.py
@@ -19,7 +19,17 @@ admin.site.register(TypeSensor)
 admin.site.register(Sensor)
 admin.site.register(MathModel)
 admin.site.register(Requirement)
-admin.site.register(Report)
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ("sensor", "value", "time")
+    search_fields = ("sensor__name", "time")
+    list_filter = ("sensor", "time")
+
+    ordering = ("-time",)
+
+
 admin.site.register(SensorInMathModel)
 admin.site.register(Constant)
 admin.site.register(MathModelResult)
