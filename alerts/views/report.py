74f3f0ee8a8e3630c0daffb47fd8b2843170d306
leo <fmleonardo@protonmail.com>
2024-10-08 11:13:44 -0300

    feat: adapt report format, calculate math models when conditions are met
    stations can now send an iso timestamp, for compatibility, if this is not set the default value (timezone.now) is used

--- alerts/views/report.py
+++ alerts/views/report.py
@@ -1,11 +1,13 @@
 import json
+from datetime import datetime
 
+from django.db.models import Q
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
-from alerts.models import Report, Station
+from alerts.models import IntermediaryRequirement, Reading, Station, Report
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -15,6 +17,7 @@ class ReportView(View):
         """
         {
           "chipid":185249135999496,
+          "time":"2024-01-01T00:00:00",
           "readings": [
             { "sensor_name": "dht_h", "value": 57.5 },
             { "sensor_name": "dht_t", "value": 17.2 },
@@ -27,18 +30,44 @@ class ReportView(View):
 
         station = Station.objects.get(station_id=body.get("chipid"))
 
+        report = Report(station=station)
+        if body.get("time"):
+            report.time = datetime.fromisoformat(body.get("time"))
+
+        report.save()
+
+        sensors = []
+
         for reading in body.get("readings"):
             sensor = station.sensor_set.get(type__name=reading.get("sensor_name"))
-            Report.objects.create(sensor=sensor, value=float(reading.get("value")))
+            sensors.append(sensor)
 
-        return JsonResponse({"message": "ok"})
+            reading = Reading(
+                sensor=sensor, value=float(reading.get("value")), report=report
+            )
+            if report.time:
+                reading.time = None
+
+            reading.save()
+
+        requirements = IntermediaryRequirement.objects.filter(
+            requirements__sensor__in=sensors
+        ).distinct()
+
+        for requirement in requirements:
+            if requirement.validate():
+                # Caso os requerimentos sejam válidos, o modelo matemático é calculado usando os valores da estação
+                # a função retorna o resultado do modelo matemático, mas não é utilizada, apenas salva no banco de dados
+                requirement.math_model.evaluate_by_station(station)
+
+        return JsonResponse({"message": "ok"}, status=200)
 
 
 class LastReport(View):
     def get(self, request, station_chip_id):
         station = Station.objects.get(station_id=station_chip_id)
         sensor = station.sensor_set.last()
-        last_report = sensor.report_set.order_by("-time").first()
+        last_report = sensor.reading_set.order_by("-time").first()
 
         return JsonResponse(
             {
