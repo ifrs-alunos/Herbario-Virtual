caf25ddfdca24a485b748661b87db99045b38002
leo <fmleonardo@protonmail.com>
2024-11-05 23:54:32 -0300

    feat: remove unused whatsapp integration code

--- accounts/models/profile.py
+++ accounts/models/profile.py
@@ -5,15 +5,35 @@ from .solicitation import Solicitation
 
 class Profile(models.Model):
     """Essa classe define um perfil de um usuário"""
-    
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, verbose_name="Nome completo", help_text="Não utilize abreviações nos sobrenomes.")
-    institution = models.CharField(max_length=150, verbose_name="Instituição de trabalho e/ou estudo", help_text="Caso possuia mais de uma, escreva a que atua principalmente.")
-    role = models.CharField(max_length=100, verbose_name="Cargo de ofício", help_text="Utilize como base seu cargo na instituição citada.")
-    phone = models.CharField(unique=True, max_length=11, verbose_name="Telefone fixo ou celular", help_text="Informe o DDD e, em seguida, seu número.")
-    cpf = models.CharField(max_length=11, verbose_name="CPF", help_text="Informe o CPF somente com números.")
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Nome completo",
+        help_text="Não utilize abreviações nos sobrenomes.",
+    )
+    institution = models.CharField(
+        max_length=150,
+        verbose_name="Instituição de trabalho e/ou estudo",
+        help_text="Caso possuia mais de uma, escreva a que atua principalmente.",
+    )
+    role = models.CharField(
+        max_length=100,
+        verbose_name="Cargo de ofício",
+        help_text="Utilize como base seu cargo na instituição citada.",
+    )
+    phone = models.CharField(
+        unique=True,
+        max_length=11,
+        verbose_name="Telefone fixo ou celular",
+        help_text="Informe o DDD e, em seguida, seu número.",
+    )
+    cpf = models.CharField(
+        max_length=11,
+        verbose_name="CPF",
+        help_text="Informe o CPF somente com números.",
+    )
     rg = models.CharField(max_length=10, verbose_name="RG")
-    get_messages = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
