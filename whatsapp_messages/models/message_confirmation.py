7030d0a19a52fe4db9b26ffc780631ac0ec27741
leo <fmleonardo@protonmail.com>
2024-11-06 00:04:55 -0300

    refactor: move message_confirmation model to proper app

---
+++ whatsapp_messages/models/message_confirmation.py
@@ -0,0 +1,34 @@
+import secrets
+import string
+
+from django.db import models
+from django.utils import timezone
+
+from accounts.models import Profile
+
+CODE_LENGTH = 6
+CODE_EXPIRATION_TIME = 60  # minutes
+
+
+class MessageConfirmation(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, unique=True)
+    code = models.CharField(max_length=CODE_LENGTH, null=True, blank=True)
+    verified = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def has_expired(self):
+        return (
+            timezone.now() - self.created_at
+        ).total_seconds() / 60 > CODE_EXPIRATION_TIME
+
+    @property
+    def phone_number(self):
+        return self.profile.phone
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = "".join(
+                secrets.choice(string.digits) for _ in range(CODE_LENGTH)
+            )
+        super().save(*args, **kwargs)
