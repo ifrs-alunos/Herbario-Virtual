61b4c2e72b52b10eb3c9216815a810a8b60ef78b
Leonardo Fochesato Michelon <fmleonardo@protonmail.com>
2025-07-26 11:46:03 -0300

    feature: implement admin page for MessageConfirmation

--- whatsapp_messages/admin.py
+++ whatsapp_messages/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models.message import Message
+from .models.message_confirmation import MessageConfirmation
 # Register your models here.
 
 admin.site.register(Message)
+
+@admin.register(MessageConfirmation)
+class MessageConfirmationAdmin(admin.ModelAdmin):
+    ...
