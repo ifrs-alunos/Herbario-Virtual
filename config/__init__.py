b002d45857d8824f5741dc515c197abd0d4abd1f
Kalel33 <kalel.ramos.vacaria@gmail.com>
2025-12-11 14:26:07 -0300

    Sistema de Alertas

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,8 @@
-from .celery import app as celery_app
+try:
+    from .celery import app as celery_app
+    __all__ = ('celery_app',)
+except ImportError:
+    print("Aviso: Celery não está configurado. Continuando sem Celery...")
+    celery_app = None
 
 __all__ = ('celery_app',)
